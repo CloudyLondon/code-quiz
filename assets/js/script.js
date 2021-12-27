ab7c30ed99d08f6a615a57cdc1e62bc71601d6b6
CloudyLondon <jovialcty@gmail.com>
2021-12-27 15:44:29 -0800

    add html

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// GIVEN I am taking a code quiz
+// 1. ref the start btn within my html file
+//   1(a) attach a "click" event to the "start" btn
+//   1(b) eventHandler that fires once the btn's clicked
+// THEN a timer starts and I am presented with a question
+// 2. start a timer and rander the first question
+//   2(a)
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
